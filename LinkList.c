b74fcf565b0ce8d621c49023dc2601e72da599c2
alexdreyesz <alexdreyesz@gmail.com>
2024-07-21 02:27:24 -0400

    Link List Menu

--- LinkList.c
+++ LinkList.c
@@ -1,14 +1,16 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+// Link List Struct
 typedef struct node {
     int data;
     struct node* next;
 } node; 
 
+// Print Link List
 void print_link(node* head) {   
     if(head == NULL) {
-        printf("NULL\n\n");
+        printf("NULL\n");
         return;
     }
 
@@ -19,6 +21,7 @@ void print_link(node* head) {
     print_link(head);
 }
 
+// Insert Link List Node To The Head
 void insert_head(node** head, int data) {
     node* new_node = (node*)malloc(sizeof(node));
 
@@ -28,6 +31,7 @@ void insert_head(node** head, int data) {
     *head = new_node;
 }
 
+// Insert Link List Node To The Tail
 void insert_tail(node** head, int data) {
     node* new_node = (node*)malloc(sizeof(node));
     new_node->data = data;
@@ -42,6 +46,7 @@ void insert_tail(node** head, int data) {
     temp->next = new_node; 
 }
 
+// Delete Link List Node From Head
 void delete_head(node** head) {
     if(*head == NULL) {
         return;
@@ -60,6 +65,7 @@ void delete_head(node** head) {
     free(temp);
 }
 
+// Delete Link List Node From Tail
 void delete_tail(node** head) {
     if(*head == NULL) {
         return;
@@ -87,17 +93,59 @@ void delete_tail(node** head) {
 int main() {
     node* head = NULL; 
 
-    insert_head(&head, 5);
-    insert_head(&head, 2);
-    insert_head(&head, 3);
-    insert_head(&head, 4);
+    int option = 0;
+    int data = 0;
 
-    insert_tail(&head, 10);
-    insert_tail(&head, 2);
-    insert_tail(&head, 4);
+    printf("MENU: Link List \n");
+    printf("Option 1: Insert Head \n");
+    printf("Option 2: Insert Tail \n");
+    printf("Option 3: Delete Head \n");
+    printf("Option 4: Delete Tail \n");
+    printf("Option 5: Print \n");
+    printf("Option 6: Stop Program \n");
 
-    delete_tail(&head);
-    delete_head(&head);
+    while(option != 6) {
 
-    print_link(head);
+        printf("\nChoose Your Option: ");
+        scanf("%d", &option);
+
+        // Link List Menu Options 
+        switch (option)
+        {
+        // Option 1: Insert Head Link List
+        case 1:
+            printf("Insert Node Value: ");
+            scanf("%d", &data);
+            insert_head(&head, data);
+            break;
+        // Option 2: Insert Tail Link List
+        case 2:
+            printf("Insert Node Value: ");
+            scanf("%d", &data);
+            insert_tail(&head, data);
+            break;
+        // Option 3: Delete Head Link List
+        case 3:
+            delete_head(&head);
+            break;
+        // Option 4: Delete Tail Link List
+        case 4:
+            delete_tail(&head);
+            break;
+        // Option 5: Print Link List 
+        case 5:
+            print_link(head);
+            break;
+        // Option 6: Stop Program
+        case 6:
+            printf("Closing Link List Program \n");
+            break;
+        // Default Option
+        default:
+            printf("Invalid Option \n");
+            break;
+        }
+    }
+
+    return 0;
 }
