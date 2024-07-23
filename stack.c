e75e80c353c38c1a4ee926367f50945a8a153465
alexdreyesz <alexdreyesz@gmail.com>
2024-07-23 19:11:10 -0400

    First Commit

---
+++ stack.c
@@ -0,0 +1,34 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+typedef struct node{
+    int data;
+    struct node* next;
+} node;
+
+typedef struct stack{
+    struct node* head;
+    struct node* tail;
+} stack;
+
+void     
+    }
+
+
+}
+
+void push() {
+
+}
+
+int pop() {
+
+}
+
+int main() {
+
+
+
+
+    return 0;
+}
