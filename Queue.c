ee24c8477318c4cff7414ced4a2f0a2960a62ac0
alexdreyesz <alexdreyesz@gmail.com>
2024-07-23 18:29:46 -0400

    Second Commit

--- Queue.c
+++ Queue.c
@@ -18,10 +18,12 @@ void print_queue(queue* qline) {
     }
     
     node* current = qline->head;
+
     while (current != NULL) {
         printf("%d -> ", current->data);
         current = current->next;
     }
+
     printf("NULL\n\n");
 }
 
@@ -41,22 +43,69 @@ void enqueue(queue* qline, int data) {
     qline->tail = new_node;
 }
 
-void deque(queue* qline, int data) {
-    
+int deque(queue* qline) {
+    if(qline->head == NULL) {
+        return 0;
+    }
+
+    node* temp = qline->head;
+
+    int value = temp->data;
+
+    qline->head = qline->head->next;
+
+    if (qline->head == NULL) {
+        qline->tail = NULL;
+    }
+
+    free(temp);
+
+    return value;
 }
 
 int main() {
-
     queue* qline = (queue*)malloc(sizeof(queue));
-
     qline->head = NULL;
     qline->tail = NULL; 
 
-    printf("\n");
+    int option = 0;
+    int data = 0;
 
-    enqueue(qline, 3);
-    enqueue(qline, 1);
-    enqueue(qline, 7);
+    printf("MENU: Queue \n");
+    printf("Option 1: Enqueue \n");
+    printf("Option 2: Dequeue \n");
+    printf("Option 3: Print \n");
+    printf("Option 4: Exit Program \n");
 
-    print_queue(qline);
+    while(option != 4) {
+
+        printf("\nChoose Your Option: ");
+        scanf("%d", &option);
+
+        // Queue Menu Options 
+        switch (option) {
+        // Option 1: Enqueue Node
+        case 1:
+            printf("Insert Node Value: ");
+            scanf("%d", &data);
+            enqueue(qline, data);
+            break;
+        // Option 2: Dequeue Node
+        case 2:
+            printf("Dequeue %d\n", deque(qline));
+            break;
+        // Option 3: Print Queue
+        case 3:
+            print_queue(qline);
+            break;
+        // Option 4: Exit Program
+        case 4:
+            printf("Closing Program \n");
+            break;
+        // Default Option
+        default:
+            printf("Invalid Option \n");
+            break;
+        }
+    }
 }
