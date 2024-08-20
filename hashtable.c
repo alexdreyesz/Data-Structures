42110b8f8bed46f2019cdcfd5a2ee07bfd9614d7
alexdreyesz <alexdreyesz@gmail.com>
2024-08-20 18:57:39 -0400

    Update

--- hashtable.c
+++ hashtable.c
@@ -4,6 +4,7 @@
 
 int HashTableSize = 0;
 int HashTableCapacity = 5;
+int BucketSize = 10;
 
 typedef struct HashTable {
     char *word;
@@ -65,11 +66,11 @@ int createKey(char *word) {
     int value = 0;
    
     for(int i = 0; i < strlen(word); i++) {
-        // Make The Range From "a" - "b" To Start From 0
+        // Make The Range From "a" - "z" To Start From 0
         value = value - 97;
         
-        // If Range Is From 'A' - 'B' Accomodate The Value By 32
-        if(word[i] >= 'A' || word[i] <= 'Z') {
+        // If Range Is From 'A' - 'Z' Accomodate The Value By 32
+        if(word[i] >= 'A' && word[i] <= 'Z') {
             value = value + 32;
         }
 
@@ -78,6 +79,18 @@ int createKey(char *word) {
     }
 
     return value;
+}
+
+HashTable* linearProbing(){
+
+}
+
+HashTable* quadraticProbing() {
+
+}
+
+HashTable* chaining() {
+
 }
 
 int main() {
@@ -93,10 +106,73 @@ int main() {
     hash_table = create_node(hash_table, "Car");
     hash_table = create_node(hash_table, "Dog");
     hash_table = create_node(hash_table, "Airplane");
+    hash_table = create_node(hash_table, "Rock");
+    hash_table = create_node(hash_table, "Fire");
+    hash_table = create_node(hash_table, "Water");
+    hash_table = create_node(hash_table, "Cat");
+    hash_table = create_node(hash_table, "Wood");
+
+    printf("\n");
 
     // Print Each Table 
     for(int i = 0; i < HashTableSize; i++) {
         printf("word: %s, Key: %d\n", hash_table[i].word, hash_table[i].key);
+    }
+
+    printf("\n\n");
+
+    // Create Linear Probing Bucket
+    HashTable *linear_hash_table = (HashTable*)malloc(BucketSize * sizeof(HashTable));
+
+    if (linear_hash_table == NULL) {
+        printf("Initial Memory Allocation Failed.\n");
+        return 1;
+    }
+
+    linear_hash_table = linearProbing(hash_table, HashTableSize);
+
+    for(int i = 0; i < BucketSize; i++) {
+        if(linear_hash_table == NULL) {
+            printf("Bucket: %d  NULL", i);
+        }
+
+        printf("Bucket: %d  Word: %s", i, linear_hash_table->word);
+    }
+
+    // Create Quadratic Probing Bucket
+    HashTable *quadratic_hash_table = (HashTable*)malloc(BucketSize * sizeof(HashTable));
+
+    if (hash_table == NULL) {
+        printf("Initial Memory Allocation Failed.\n");
+        return 1;
+    }
+
+    quadratic_hash_table = quadraticProbing(hash_table, HashTableSize);
+
+    for(int i = 0; i < BucketSize; i++) {
+        if(quadratic_hash_table == NULL) {
+            printf("Bucket: %d  NULL", i);
+        }
+
+        printf("Bucket: %d  Word: %s", i, quadratic_hash_table->word);
+    }
+
+    // Create Chaining Bucket
+    HashTable *chaining_hash_table = (HashTable*)malloc(BucketSize * sizeof(HashTable));
+
+    if (hash_table == NULL) {
+        printf("Initial Memory Allocation Failed.\n");
+        return 1;
+    }
+
+    chaining_hash_table = chaining(hash_table, HashTableSize);
+
+    for(int i = 0; i < BucketSize; i++) {
+        if(chaining_hash_table == NULL) {
+            printf("Bucket: %d  NULL", i);
+        }
+
+        printf("Bucket: %d  Word: %s", i, chaining_hash_table->word);
     }
 
     // Free Allocated Memory For Each Node Word
