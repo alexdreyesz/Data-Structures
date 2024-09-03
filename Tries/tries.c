b5f2bec9a7d4e7ffee2910710c703de21447d856
alexdreyesz <alexdreyesz@gmail.com>
2024-09-02 22:53:24 -0400

    Update

---
+++ Tries/tries.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdbool.h>
+#include <string.h>
+
+#define NUM_CHARS 256
+
+typedef struct trienode {
+    struct tirenode *children[NUM_CHARS];
+    bool flag;
+} trienode;
+
+int main() {
+
+    
+
+    return 0;
+}
